b2a80fea5c26be48fca6c07d9c81f4cf402a7717
Matthew Thompson <matthewbot@gmail.com>
2010-03-27 20:29:18 -0400

    Tweaked startup script to run additional startup scripts called "chumby_startup.sh" in user program folders

--- filesystem/ext2/files/startup.sh
+++ filesystem/ext2/files/startup.sh
@@ -1,4 +1,15 @@
 #!/bin/sh
 cd /mnt/kiss
-./run_qt_app.sh ./cbcui -qws
+
+./run_qt_app.sh ./cbcui -qws &
+
+if [ ! -e /tmp/cbc_run_startup ]; then
+  touch /tmp/cbc_run_startup
+  for codedir in /mnt/user/code/*; do
+    script="$codedir/chumby_startup.sh"
+    if [ -e "$script" ]; then
+      sh $script
+    fi
+  done
+fi
 
